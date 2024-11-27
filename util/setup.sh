13e9526b0818ef6c06e7bc80235fc052178af696
Gerard Heshusius <gh.heshusius@gmail.com>
2024-11-27 15:15:49 +0100

    feat(setup.sh): Setup chezmoi

--- util/setup.sh
+++ util/setup.sh
@@ -2,11 +2,10 @@
 
 set -ex
 
-install_brew() {
-	command -v brew && return
+setup_brew() {
+	command -v brew && return 0
 
-	dpkg-query -W -f='${status}' build-essential || exit $?
-	echo "" # Above command doesn't print a new line
+	dpkg-query -W -f='${status}\n' build-essential || exit $?
 	command -v curl || exit $?
 	command -v git || exit $?
 	command -v ps || exit $?
@@ -16,6 +15,16 @@ install_brew() {
 	if [ ! -d $BREW_PREFIX ]; then
 		git clone --depth=1 https://github.com/Homebrew/brew $BREW_PREFIX
 	fi
+
+    export PATH="$HOME/.linuxbrew/Homebrew/bin:$HOME/.linuxbrew/Homebrew/sbin:$PATH"
+    eval "$($HOME/.linuxbrew/Homebrew/bin/brew shellenv)"
 }
 
-install_brew
+setup_chezmoi() {
+    command -v chezmoi && return 0
+
+	brew install chezmoi
+}
+
+setup_brew
+setup_chezmoi
