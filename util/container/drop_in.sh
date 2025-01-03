1a1c11dea8cb61b1b4058f45b8bc0c0de0857afd
Gerard Heshusius <gh.heshusius@gmail.com>
2025-01-03 12:20:59 +0100

    feat(util.container): Copy in dotfiles folder so that it is ethereal

--- util/container/drop_in.sh
+++ util/container/drop_in.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-podman run \
-	--env-file .env \
-	--rm \
-	-it \
-	-v "${HOME}"/dotfiles:/home/testuser/dotfiles \
-	dotfiles-test:latest \
-	bash
+CONTAINER_NAME="dotfiles-test:latest"
+LOCAL_DIR="${HOME}/dotfiles"
+CONTIANER_DIR="/home/testuser/dotfiles"
+
+CONTAINER_ID=$(
+	podman create \
+		--env-file .env \
+		--interactive \
+		--tty \
+		--rm \
+		"${CONTAINER_NAME}" \
+)
+
+podman cp "${LOCAL_DIR}" "${CONTAINER_ID}":"${CONTIANER_DIR}"
+
+podman start \
+	--attach \
+	--interactive \
+	"${CONTAINER_ID}"
--- util/container/run.sh
+++ util/container/run.sh
@@ -2,10 +2,23 @@
 
 REPO_PATH="${REPO_PATH:-${HOME}/dotfiles}"
 
-podman run \
-	--entrypoint /bin/bash \
-	--env-file .env \
-	--rm \
-	-v "${REPO_PATH}":/home/testuser/dotfiles \
-	dotfiles-test:latest \
-	-c "./dotfiles/util/setup.sh; ./dotfiles/util/validate.sh"
+CONTAINER_NAME="dotfiles-test:latest"
+LOCAL_DIR="${HOME}/dotfiles"
+CONTIANER_DIR="/home/testuser/dotfiles"
+
+CONTAINER_ID=$(
+	podman create \
+		--env-file .env \
+		--interactive \
+		--tty \
+		--rm \
+		--entrypoint /bin/bash \
+		"${CONTAINER_NAME}" \
+		-c "${CONTIANER_DIR}/util/setup.sh; ${CONTIANER_DIR}/util/validate.sh"
+)
+
+podman cp "${LOCAL_DIR}" "${CONTAINER_ID}":"${CONTIANER_DIR}"
+
+podman start \
+	--attach \
+	"${CONTAINER_ID}"
