f0d3f4728e917a534de2a6e10ae36caf35a14ea2
Gerard Heshusius <gh.heshusius@gmail.com>
2025-01-12 17:12:47 +0100

    feat(automation.setup): ssh.sh: Add setup for private github key

--- automation/setup_secrets.sh
+++ automation/setup_secrets.sh
@@ -7,12 +7,15 @@ fi
 
 bw login
 
+bw sync
+
 BW_SESSION=$(bw unlock --raw)
 
-echo "\
-GPG_PASSPHRASE=$(bw get password "GPG" --session "${BW_SESSION}")
-GPG_PRIVATE_KEY=$(bw get notes "GPG Private Key" --session "${BW_SESSION}" | base64 --wrap=0)
-" > .env
+{
+    echo "GPG_PASSPHRASE=$(bw get password "GPG" --session "${BW_SESSION}")"
+    echo "GPG_PRIVATE_KEY=$(bw get notes "GPG Private Key" --session "${BW_SESSION}" | base64 --wrap=0)"
+    echo "SSH_GITHUB_PRIVATE=$(bw get notes "ssh.github.private" --session "${BW_SESSION}" | base64 --wrap=0)"
+} > .env
 
 chmod 600 .env
 
