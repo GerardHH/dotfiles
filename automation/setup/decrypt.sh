48da061125d59c84a03da34da1ad7cc36350ee89
Gerard Heshusius <gh.heshusius@gmail.com>
2025-03-03 16:11:33 +0100

    feat: Move encrypted files from encrypted to home

--- automation/setup/decrypt.sh
+++ automation/setup/decrypt.sh
@@ -11,7 +11,8 @@ if [[ -z "$(gpg --list-secret-keys)" ]]; then
 fi
 
 if [[ -z "${ROOT_DIR}" ]]; then
-	echo "Warning: ROOT_DIR not set"
+	echo "Error: ROOT_DIR not set"
+	exit 1
 fi
 
 if [[ -f "${ROOT_DIR}"/.env ]]; then
@@ -23,20 +24,13 @@ if [[ -z "${GPG_PASSPHRASE}" ]]; then
 	exit 1
 fi
 
-if [[ -z "${ROOT_DIR}" ]]; then
-	echo "Error: ROOT_DIR not set"
-	exit 1
-fi
-
-SOURCE_DIR="${ROOT_DIR}/encrypted"
-TARGET_DIR="${ROOT_DIR}/home"
+SOURCE_DIR="${ROOT_DIR}/home"
 
 echo "Source dir: ${SOURCE_DIR}"
-echo "Target dir: ${TARGET_DIR}"
 
 find "${SOURCE_DIR}" -type f -name "*.gpg" | while read -r file; do
 	rel_path="${file#"${SOURCE_DIR}"/}"
-	dest_path="${TARGET_DIR}/${rel_path}"
+	dest_path="${SOURCE_DIR}/${rel_path}"
 	dest_path="${dest_path%.gpg}"
 
 	echo "Decrypt: '${file}' into '${dest_path}'"
@@ -44,12 +38,6 @@ find "${SOURCE_DIR}" -type f -name "*.gpg" | while read -r file; do
 	if [[ -f "${dest_path}" ]]; then
 		echo "Warning: Already exists, skipping"
 		continue
-	fi
-
-	dest_dir=$(dirname "${dest_path}")
-	if [[ ! -d "${dest_dir}" ]]; then
-		echo "Warning: Parent directory doesn't exist, creating"
-		mkdir -p "${dest_dir}"
 	fi
 
 	if gpg --batch --yes --pinentry-mode loopback --passphrase "${GPG_PASSPHRASE}" --output "${dest_path}" --decrypt "${file}"; then
