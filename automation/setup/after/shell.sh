7d8029e7bfcfc2108bcb2ae7e9f8fc1a1c6a82db
Gerard Heshusius <gh.heshusius@gmail.com>
2025-03-09 18:02:02 +0100

    fix(automation.setup.after.shell.sh): Make sure there is a newline after script execution

--- automation/setup/after/shell.sh
+++ automation/setup/after/shell.sh
@@ -4,12 +4,15 @@
 source "${HOME}/dotfiles/automation/util.sh"
 
 if ! command -v zsh; then
-    echo "Error: zsh not found"
-    exit 1
+	echo "Error: zsh not found"
+	exit 1
 fi
 
 echo "Source .zshrc"
 if ! zsh -c "source ${HOME}/.zshrc"; then
-    echo "Error: zsh did not source .zshrc without problems"
-    exit 1
+	echo "Error: zsh did not source .zshrc without problems"
+	exit 1
 fi
+# Make sure there's a new line at end of this scripts output
+# util.sh::execute_scripts expects that
+echo ""
