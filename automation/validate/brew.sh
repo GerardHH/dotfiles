10ce0706201a27a5decf6be298ef789bc7f5fdda
Gerard Heshusius <gh.heshusius@gmail.com>
2025-03-14 15:29:36 +0100

    fix(automation.validate.brew.sh): Remove unused .env source

--- automation/validate/brew.sh
+++ automation/validate/brew.sh
@@ -3,10 +3,6 @@
 #shellcheck source=./automation/util.sh
 source "${HOME}/dotfiles/automation/util.sh"
 
-if [[ -f "${AUTO_DIR}"/.env ]]; then
-	source "${AUTO_DIR}"/.env
-fi
-
 if ! command -v brew && brew install hello && hello; then
 	echo "Failed to detect brew, install or run hello program"
 	exit 1
