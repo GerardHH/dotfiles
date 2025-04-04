2f1e72d2eca32538b904a9a3b780e6ea3fb1006a
Gerard Heshusius <gh.heshusius@gmail.com>
2025-04-04 10:55:05 +0200

    fix(automation.source_brew.sh): Use echo instead of log_error

--- automation/source_brew.sh
+++ automation/source_brew.sh
@@ -3,21 +3,21 @@
 BREW_PREFIX=/home/linuxbrew/.linuxbrew
 
 if [[ ! -d "${BREW_PREFIX}" ]]; then
-	log_error "Cannot find brew installation, please install using the official way"
+	echo "Cannot find brew installation, please install using the official way"
 	exit 1
 fi
 
 BREW_PATHS="${BREW_PREFIX}/bin:${BREW_PREFIX}/sbin"
 
 if [[ "${PATH}" != *"${BREW_PATHS}"* ]]; then
-    export PATH="${BREW_PREFIX}/bin:${BREW_PREFIX}/sbin:${PATH}"
-    eval "$(brew shellenv)"
+	export PATH="${BREW_PREFIX}/bin:${BREW_PREFIX}/sbin:${PATH}"
+	eval "$(brew shellenv)"
 
-    # HACK If init process is not systemd, assume we're in a container
-    # Prefer applications installed by container over those of linuxbrew
-    if [ "$(ps -p 1 -o comm=)" != "systemd" ]; then
-        export PATH=/usr/bin:${PATH}
-    fi
+	# HACK If init process is not systemd, assume we're in a container
+	# Prefer applications installed by container over those of linuxbrew
+	if [ "$(ps -p 1 -o comm=)" != "systemd" ]; then
+		export PATH=/usr/bin:${PATH}
+	fi
 
-    export HOMEBREW_NO_ENV_HINTS=1
+	export HOMEBREW_NO_ENV_HINTS=1
 fi
