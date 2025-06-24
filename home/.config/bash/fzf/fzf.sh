aa38f0c944e34f7710daea6865045afc1abef3a2
Gerard Heshusius <gh.heshusius@gmail.com>
2025-06-24 09:59:55 +0200

    feat(home.config.bash.fzf.fzf.sh): Make preview window 60% height and allow preview results to wrap

--- home/.config/bash/fzf/fzf.sh
+++ home/.config/bash/fzf/fzf.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 export FZF_DEFAULT_OPTS=" \
---height=40% \
+--height=60% \
 --border \
 --layout=reverse \
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 # See https://github.com/lincheney/fzf-tab-completion/issues/85 on why {1} is needed.
-export FZF_COMPLETION_OPTS='--preview "source $HOME/.config/bash/fzf/fzf-preview.sh; __fzf_preview {1}" --preview-window=right:60%'
+export FZF_COMPLETION_OPTS='--preview "source $HOME/.config/bash/fzf/fzf-preview.sh; echo {1}; __fzf_preview {1}" --preview-window=right:60%:wrap'
 
 FZF_TAB_DIR="$HOME/.config/bash/fzf/fzf-tab-completion"
 [ -f "$FZF_TAB_DIR/bash/fzf-bash-completion.sh" ] && source "$FZF_TAB_DIR/bash/fzf-bash-completion.sh"
